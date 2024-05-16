178eebe218b640c7531f7b9ceacc9b6972c289ce
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-05-16 12:44:24 +0200

    cicli

---
+++ Giovedi_16/Cicli.java
@@ -0,0 +1,21 @@
+public class Cicli {
+    public static void main(String[] args) {
+        //While
+        int i = 0;//inizializzazione della variabile di controllo;
+        while (i<0) {
+            i++;// incremento della variabile di controllo;
+            System.out.println("Valore di i: "+i);
+        }
+
+        //Do-While
+        int x = 0;
+        do {
+            x++;// incremento della variabile di controllo;
+            System.out.println("Valore di x: " + x);
+        } while (x<0);
+    }
+
+}
+
+
+
