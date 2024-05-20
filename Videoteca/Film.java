ada7061c325f790963348301e97d6ca1fe91223a
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-05-20 17:39:03 +0200

    creazione dei file

---
+++ Videoteca/Film.java
@@ -0,0 +1,16 @@
+public class Film {
+    String title;
+    int year;
+
+    public Film(String titolo, int anno){
+        title = titolo;
+        year = anno;
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    public int getYear(){
+        return year;
+    }
+}
