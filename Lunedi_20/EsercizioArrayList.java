a6c5f75b3ff774485e5e70a7d6b8b07ec9fff5e6
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-05-20 12:57:18 +0200

    Esercizio array list ottimizzato

--- Lunedi_20/EsercizioArrayList.java
+++ Lunedi_20/EsercizioArrayList.java
@@ -13,55 +13,93 @@ public class EsercizioArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
         Scanner scannerS = new Scanner(System.in);
+        Scanner scannerN = new Scanner(System.in);
         boolean trovato = false;
+        boolean ricerca =true;
+        boolean rimozione = true;
+        String cercato;
+        int numero;
 
-        System.out.println("Inserisci cinque nomi da inserire nell'array list.");
+        System.out.println("Inerisci il numero di nomi da inserire.");
+        numero = scannerN.nextInt();
+        System.out.println();
+        System.out.println("Inserisci "+numero+ " nomi da inserire nell'array list.");
         //Aggiunta
-        for(int i = 0; i<5;i++){
-            System.out.println("Inserisci nome ["+i+"] da inserire nell'array list.");
+        for(int i = 0; i<numero;i++){
+            System.out.println("Inserisci nome ["+(i+1)+"] da inserire nell'array list.");
             names.add(scannerS.nextLine());
         }
+
+        System.out.println();
+
         //Stampa
         for(String name: names){
             System.out.println(names.indexOf(name)+" "+name);
         }
-        //Ricerca
-        System.out.println("Inserisci un nome da cercare nell'array list.");
-        String cercato = scannerS.nextLine();
-        for(String name: names){
-            if (name.toLowerCase().equals(cercato.toLowerCase())) {
-                trovato = true;
-            }else{
-                trovato = false;
+
+        System.out.println();
+
+        while (ricerca) {
+            // Ricerca
+            System.out.println("Inserisci un nome da cercare nell'array list.");
+            trovato = false;
+            cercato = scannerS.nextLine();
+            for (String name : names) {
+                if (name.toLowerCase().equals(cercato.toLowerCase())) {
+                    trovato = true;
+                    // names.remove(names.indexOf(name));
+                    break;
+                } else {
+                    trovato = false;
+                }
+            }
+            if (trovato) {
+                System.out.println(cercato + " é presente.");
+            } else {
+                System.out.println(cercato + " non é presente.");
+            }
+            System.out.println("Vuoi effettuare una nuova ricerca? [y] Si alrtrimenti premi invio.");
+            if (scannerS.nextLine().toLowerCase().trim().equals("y")) {
+                ricerca = true;
+            } else {
+                ricerca = false;
             }
         }
-        if (trovato) {
-            System.out.println(cercato + " é all'interno dell'arry list");
-        }else{
-            System.out.println(cercato + " non é all'interno dell'arry list");
-        }
+        
 
         //Rimozione
-        System.out.println("Inserire un nome da rimuovere ");
-        trovato = false;
-        cercato = scannerS.nextLine();
-        for(String name: names){
-            if (name.toLowerCase().equals(cercato.toLowerCase())) {
-                trovato = true;
-                break;
-            }else{
-                trovato = false;
+        while (rimozione){
+            System.out.println("Inserire un nome da rimuovere ");
+            trovato = false;
+            cercato = scannerS.nextLine();
+            for (String name : names) {
+                if (name.toLowerCase().equals(cercato.toLowerCase())) {
+                    trovato = true;
+                    names.remove(names.indexOf(name));
+                    break;
+                } else {
+                    trovato = false;
+                }
             }
-        }
-        if (trovato) {
-            System.out.println(cercato+" é stato rimosso.");
-        }else{
-            System.out.println(cercato + " non é presente e non é stato rimosso.");
-        }
+            if (trovato) {
+                System.out.println(cercato + " é stato rimosso.");
+            } else {
+                System.out.println(cercato + " non é presente e non é stato rimosso.");
+            }
+            System.out.println("Vuoi effettuare una nuova rimozione? [y] Si alrtrimenti premi invio.");
+            if (scannerS.nextLine().toLowerCase().trim().equals("y")) {
+                rimozione = true;
+            } else {
+                rimozione = false;
+            }
 
+        }
+        
         // Stampa aggiornata
         for (String name : names) {
-            System.out.println(names.indexOf(name) + " " + name);
+            System.out.println("["+names.indexOf(name) + "] " + name);
         }
+        scannerS.close();
+        scannerN.close();
     }
 }
