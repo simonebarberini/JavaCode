2ab1b000b41bbc96bda9b0037defb0a89c70fa59
simonebarberini <147926684+simonebarberini@users.noreply.github.com>
2024-05-20 10:08:49 +0200

    Matematica

---
+++ Lunedi_20/Matematica.java
@@ -0,0 +1,11 @@
+public class Matematica {
+    public static void main(String[] args) {
+        Math.min(5, 10);//restituisce il minimo tra i valori passati
+        Math.max(5, 10);//restituisce il massimo tra i valori passati
+        Math.sqrt(64);//restituisce la radice quadrata di x
+        Math.abs(-4.7);
+        Math.random();//restituisce un numero casuale float da 0.0 a 1.0
+        int randomNum = (int)(Math.random()*101);//Numero casuale tra 0 e 100
+        //Vedere anche la classe Random
+    }
+}
